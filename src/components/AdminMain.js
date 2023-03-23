c2c0fe8dc8bb45793515cd234ad6c60245efda8f
Blazmian <demionfeick@gmail.com>
2023-03-23 00:02:31 -0700

    Admin Interface

--- src/components/AdminMain.js
+++ src/components/AdminMain.js
@@ -1,16 +1,28 @@
+import { Col, Container, Row } from "react-bootstrap"
 import { Route, Routes } from "react-router-dom"
 import AdminSideBar from "./AdminSideBar"
+import Users from "./api/Admin/Users/UsersInterface"
 
 const AdminMain = () => {
     return (
         <>
-            <AdminSideBar />
-            <Routes>
-                <Route path="/reportes/*" />
-                <Route path="/usuarios/*" />
-                <Route path="/material/*" />
-                <Route path="/caseteros/*" />
-            </Routes>
+            <Container fluid='true'>
+                <Row>
+                    <Col lg='auto' md='auto' sm='auto' xl='auto' xs='auto' xxl='auto'>
+                        <AdminSideBar />
+                    </Col>
+                    <Col>
+                        <Container fluid='true'>
+                            <Routes>
+                                <Route path="/reportes/*" />
+                                <Route path="/usuarios/*" element={<Users />} />
+                                <Route path="/material/*" />
+                                <Route path="/caseteros/*" />
+                            </Routes>
+                        </Container>
+                    </Col>
+                </Row>
+            </Container>
         </>
     )
 }
--- src/components/AdminSideBar.js
+++ src/components/AdminSideBar.js
@@ -4,39 +4,31 @@ import { NavLink } from "react-router-dom"
 const AdminSideBar = () => {
     return (
         <div style={{ display: 'flex', height: '100vh', overflow: 'scroll initial' }}>
-            <CDBSidebar backgroundColor="#1D3A69">
-                <CDBSidebarHeader prefix={<i className="fa fa-bars fa-large"></i>}>
-                    <a href="/" className="text-decoration-none" style={{ color: 'inherit' }}>
-                        <strong>TECNM</strong>
-                    </a>
+            <CDBSidebar backgroundColor="#333" textColor="#fff" toggled>
+                <CDBSidebarHeader prefix={<i className="fa fa-bars" />}>
+                    <strong>TECNM</strong>
                 </CDBSidebarHeader>
 
                 <CDBSidebarContent className="sidebar-content">
                     <CDBSidebarMenu>
-                        <NavLink exact to="admin/" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem icon="columns">Reportes</CDBSidebarMenuItem>
+                        <NavLink to="/admin/reportes" activeClassName="activeClicked">
+                            <CDBSidebarMenuItem icon="book">Reportes</CDBSidebarMenuItem>
                         </NavLink>
-                        <NavLink exact to="admin/tables" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem icon="user">Usuarios</CDBSidebarMenuItem>
+                        <NavLink to="/admin/usuarios" activeClassName="activeClicked">
+                            <CDBSidebarMenuItem icon="users">Usuarios</CDBSidebarMenuItem>
                         </NavLink>
-                        <NavLink exact to="admin/profile" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem icon="user">Material</CDBSidebarMenuItem>
-                        </NavLink>
-                        <NavLink exact to="admin/analytics" activeClassName="activeClicked">
-                            <CDBSidebarMenuItem icon="chart-line">Casetero</CDBSidebarMenuItem>
+                        <NavLink to="/admin/equipo" activeClassName="activeClicked">
+                            <CDBSidebarMenuItem icon="archive">Equipo</CDBSidebarMenuItem>
                         </NavLink>
                     </CDBSidebarMenu>
                 </CDBSidebarContent>
 
                 <CDBSidebarFooter style={{ textAlign: 'center' }}>
-                    <div
-                        className="sidebar-btn-wrapper"
-                        style={{
-                            padding: '20px 5px',
-                        }}
-                    >
-                        Campus Hermosillo
-                    </div>
+                    <CDBSidebarMenu>
+                        <NavLink to="/logout">
+                            <CDBSidebarMenuItem icon='sign-out-alt'>Cerrar Sesión</CDBSidebarMenuItem>
+                        </NavLink>
+                    </CDBSidebarMenu>
                 </CDBSidebarFooter>
 
             </CDBSidebar>
